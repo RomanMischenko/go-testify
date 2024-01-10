3a3daf5ecf6dd646bedd0c853c1f4031f9a49b49
RomanMischenko <roma_mishenko@mail.ru>
2024-01-10 11:18:58 +0300

    add TestMainHandlerWhenCountMoreThanTotal

--- precode_test.go
+++ precode_test.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"testing"
-	"strings"
 	"net/http/httptest"
+	"strings"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	//"github.com/stretchr/testify/require"
 )
 
-
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
     responseRecorder := httptest.NewRecorder()
     handler := http.HandlerFunc(mainHandle)
     handler.ServeHTTP(responseRecorder, req)
-	
-    // здесь нужно добавить необходимые проверки
-	//
+
+	expCount := 4
+	expCode := http.StatusOK
+	expAnswer := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
+
+	assert.Equal(t, expCode, responseRecorder.Code)
+
+	serverResponse := responseRecorder.Body.String()
+	serverResponseCount := strings.Split(serverResponse, ",")
+	assert.Equal(t, expAnswer, serverResponse)
+	assert.Len(t, serverResponseCount, expCount)
 }
