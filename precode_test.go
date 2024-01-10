0405daa51f9e73465f55f0683d97a522c3830eb9
RomanMischenko <roma_mishenko@mail.ru>
2024-01-10 11:02:29 +0300

    add packge testify

--- precode_test.go
+++ precode_test.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"net/http"
 	"testing"
+	"strings"
 	"net/http/httptest"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
     totalCount := 4
-    //req := ... // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
     responseRecorder := httptest.NewRecorder()
     handler := http.HandlerFunc(mainHandle)
     handler.ServeHTTP(responseRecorder, req)
-
+	
     // здесь нужно добавить необходимые проверки
+	//
 }
